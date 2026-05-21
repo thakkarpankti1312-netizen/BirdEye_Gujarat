a9c54177fec5b7d464c955d5f18538086016d238
Pankti Thakkar <thakkarpankti1312@gmail.com>
2026-05-21 21:29:33 +0530

    Initial upload

---
+++ mongodb/upload.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+import pandas as pd
+
+df = pd.read_csv("birds_info.csv")
+data = df.to_dict(orient="records")
+
+client = MongoClient(("mongodb+srv://thakkarpankti1312_db_user:Bird123@birdeye.1iwxlsx.mongodb.net/?retryWrites=true&w=majority&appName=BIRDEYE"))
+
+db = client["BirdEyeDB"]
+collection = db["birds"]
+
+collection.insert_many(data)
+
+print("Data uploaded successfully 🚀")
